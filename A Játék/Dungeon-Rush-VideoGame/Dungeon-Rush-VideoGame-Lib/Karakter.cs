461927ccd5a2b2940d839f38b2866baa8d6431d9
KarolyNeu <toth.karoly.attila@njit.hu>
2024-05-28 10:45:02 +0200

    A játék  kezdete
    Megcsináltam ,hogy  kezdésnek bekérje a játékos nevét classokat csináltam neki.

--- A Játék/Dungeon-Rush-VideoGame/Dungeon-Rush-VideoGame-Lib/Karakter.cs
+++ A Játék/Dungeon-Rush-VideoGame/Dungeon-Rush-VideoGame-Lib/Karakter.cs
@@ -4,18 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Dungeon_Rush_Lib
+namespace Dungeon_Rush_VideoGame_Lib
 {
-    public class Karakter
+    class Jatekos
     {
-        public string Nev { get; init; }
-        public int Level { get; set; } = 1;
-        public int XP { get; set; }
-        public int XPKoviszint { get; set; } = 10;
-        public int HP { get; set; } = 5;
-        public int MaxHP => Level * 5;
-        public int Arany { get; set; }
-        public int Speed { get; set; } = 1;
-        public int Damage { get; set; } = 1;
+        public string Nev;
+
+        //public int Level { get; set; } = 1;
+        //public int XP { get; set; }
+        //public int XPKoviszint { get; set; } = 10;
+        //public int HP { get; set; } = 5;
+        //public int MaxHP => Level * 5;
+        //public int Arany { get; set; }
+        //public int Speed { get; set; } = 1;
+        //public int Damage { get; set; } = 1;
     }
 }
--- A Játék/Dungeon-Rush-VideoGame/Dungeon-Rush-VideoGame/Program.cs
+++ A Játék/Dungeon-Rush-VideoGame/Dungeon-Rush-VideoGame/Program.cs
@@ -1,10 +1,7 @@
 ﻿using Dungeon_Rush_VideoGame_Lib;
 
 
-enum Direction
-{
-    Up = 0,
-    Down = 1,
-    Left = 2,
-    Right = 3,
-}
+
+
+
+
