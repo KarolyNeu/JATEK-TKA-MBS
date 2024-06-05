37f73ae7d8c723a8bad538763d7f3e76ad1cdee2
KarolyNeu <toth.karoly.attila@njit.hu>
2024-06-06 00:40:29 +0200

    1-től 3-pályáig fixelések.
    Megoldottam minden pályánál a mozgás 1-től 3-ig. Emellett a szövegeket is.

--- DungeonRush/DungeonRush/DungeonRush/Program.cs
+++ DungeonRush/DungeonRush/DungeonRush/Program.cs
@@ -16,7 +16,9 @@ namespace DungeonRush
             JatekElso();
             EgyKetto();
             JatekMasodik();
-
+            KettoHarom();
+            JatekHarmadik();
+            HaromNegy();
         }
         static void lassuKiirás(string szöveg, double szünetMásodperc)
         {
@@ -28,12 +30,12 @@ namespace DungeonRush
         }
         static void Kezdes()
         {
-            lassuKiirás("\tDungeon Rush The Videogame!", 0.2);
+            lassuKiirás("\tDungeon Rush The Videogame!", 0.1);
             Console.WriteLine();
-            lassuKiirás("\tA játékos neve: ", 0.3);
+            lassuKiirás("\tAdja meg a játékos nevét: ", 0.1);
             jelenlegiJatekos.Nev = Console.ReadLine();
             lassuKiirás("\tFelébredtél egy sötét bányában azzal a küldetéssel,\n\tlegyőzd az ördögöt és zárd vissza a pecsétjébe.", 0.1);
-            lassuKiirás("\n\tAmint készen állsz nyomj meg egy billentyűt!", 0.2);
+            lassuKiirás("\n\tAmint készen állsz nyomj meg egy billentyűt!", 0.1);
             Console.ReadKey();
             Console.Clear();
             lassuKiirás("\tMost lábra állsz és elindulsz a nehéz utadra", 0.1);
@@ -133,10 +135,10 @@ namespace DungeonRush
         }
         static void EgyKetto() { 
 
-        lassuKiirás($"\tSikerült kijutnod az első pályáról gratulálok!{jelenlegiJatekos.Nev}", 0.3);
+        lassuKiirás($"\tSikerült kijutnod az első pályáról gratulálok! {jelenlegiJatekos.Nev}", 0.1);
         Console.Clear();
-        lassuKiirás($"\tDe a küldetésednek nincsen vége. \n\tMenj tovább és ne állj meg!", 0.2);
-        lassuKiirás("\n\tAmint készen állsz nyomj meg egy billentyűt!", 0.2);
+        lassuKiirás($"\tDe a küldetésednek nincsen vége. \n\tMenj tovább és ne állj meg!", 0.1);
+        lassuKiirás("\n\tAmint készen állsz nyomj meg egy billentyűt!", 0.1);
         Console.ReadKey();
         Console.Clear();
         }
@@ -145,10 +147,10 @@ namespace DungeonRush
             bool jatekvege = true;
             // Mátrix definiálása
             int[,] palyaegy = new int[,] {
-            { 0, 1, 2, 2, 2, 2, 2, 2 , 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1 ,0 ,0, 0, 1 , 2, 2, 2, 2, 2, 1},
+            { 0, 1, 2, 2, 2, 2, 2, 2 , 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1 ,4 ,4, 4, 1 , 2, 2, 2, 2, 2, 1},
             { 0, 1, 0, 0, 0, 0, 0, 0 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0, 0, 0 , 0, 0, 0, 0, 0, 1},
             { 0, 1, 0, 0, 0, 2, 2, 1 , 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2 ,0 ,0, 0, 1 , 2, 2, 0, 0, 0, 1},
-            { 0, 1, 0, 4, 0, 0, 0, 1 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0, 0, 1 , 0, 0, 0, 4, 0, 1},
+            { 0, 1, 0, 0, 0, 0, 0, 1 , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0, 0, 1 , 0, 0, 0, 0, 0, 1},
             { 0, 1, 2, 2, 0, 0, 0, 1 , 0, 0, 0, 1, 0, 0, 0, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1 ,0 ,0, 0, 1 , 0, 0, 0, 2, 2, 1},
             { 0, 1, 0, 0, 0, 0, 0, 0 , 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 ,0 ,0, 0, 0 , 0, 0, 0, 0, 0, 1},
             { 0, 1, 0, 0, 0, 2, 2, 2 , 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1, 0, 0, 0, 2, 2, 2, 2, 2, 2, 1 ,2 ,2, 2, 2 , 2, 2, 0, 0, 0, 1},
@@ -158,8 +160,8 @@ namespace DungeonRush
 
 
             // Kocka pozíciója
-            int kockaX = 10;
-            int kockaY = 6;
+            int kockaX = 5;
+            int kockaY = 8;
 
             // Fő ciklus
             while (jatekvege)
@@ -233,6 +235,120 @@ namespace DungeonRush
                 }
                 Console.Clear();
             }
+        }
+        static void KettoHarom()
+        {
+
+            lassuKiirás($"\tAhogy látom tényleg teljesíteni fogod a küldetésed!{jelenlegiJatekos.Nev}", 0.1);
+            Console.Clear();
+            lassuKiirás($"\n\tMenj tovább és ne állj meg!", 0.1);
+            lassuKiirás("\n\tAmint készen állsz nyomj meg egy billentyűt!", 0.1);
+            Console.ReadKey();
+            Console.Clear();
+        }
+        static void JatekHarmadik()
+        {
+            bool jatekvege = true;
+            // Mátrix definiálása
+            int[,] palyaharom = new int[,] {
+{1, 2, 2, 2, 2, 2, 1, 4, 4, 4, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+{1, 0, 0, 0, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1, 0, 0, 0, 2, 2, 2, 2, 2, 2, 1, 0, 0, 0, 1, 2, 2, 0, 0, 0, 1},
+{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1},
+{1, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1, 0, 0, 0, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 2, 2, 1},
+{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+{1, 0, 0, 0, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1, 0, 0, 0, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1},
+{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+{1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 0, 0, 0, 2, 2, 2, 1}
+        };
+
+
+            // Kocka pozíciója
+            int kockaX = 35;
+            int kockaY = 8;
+
+            // Fő ciklus
+            while (jatekvege)
+            {
+                // Mátrix kiírása
+
+                for (int i = 0; i < palyaharom.GetLength(0); i++)
+                {
+                    for (int j = 0; j < palyaharom.GetLength(1); j++)
+                    {
+                        if (i == kockaY && j == kockaX)
+                        {
+                            Console.Write("X"); // Kocka karaktere
+                        }
+                        else if (palyaharom[i, j] == 1)
+                        {
+                            Console.Write("║"); // Kocka karaktere
+                        }
+                        else if (palyaharom[i, j] == 2)
+                        {
+                            Console.Write("═");
+                        }
+                        else if (palyaharom[i, j] == 4)
+                        {
+                            Console.Write("+");
+                        }
+                        else if (palyaharom[i, j] == 0)
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+
+
+
+                Console.Write("Nyomj meg egy billentyűt (w, s, d, a): ");
+                Console.WriteLine(palyaharom[kockaY, kockaX]);
+
+                char billentyu = Console.ReadKey().KeyChar;
+                Console.Clear();
+
+
+                if (billentyu == 'w')
+                {
+                    kockaY -= 1;
+                }
+                else if (billentyu == 's')
+                {
+                    kockaY += 1;
+                }
+                else if (billentyu == 'a')
+                {
+                    kockaX -= 1;
+                }
+                else if (billentyu == 'd')
+                {
+                    kockaX += 1;
+                }
+                // Határok ellenőrzése
+                if (kockaX < 0 || kockaX >= palyaharom.GetLength(1) ||
+                kockaY < 0 || kockaY >= palyaharom.GetLength(0))
+                {
+                    Console.WriteLine("Határt túllépted!");
+                    kockaX -= 10;
+                    kockaY = 6;
+                }
+                if (palyaharom[kockaY, kockaX] == 4)
+                {
+                    jatekvege = false;
+                }
+                Console.Clear();
+            }
+        }
+        static void HaromNegy()
+        {
+
+            lassuKiirás($"\tElértél a bánya végére az ÖRDÖG helyére. Innentől már nincsen vissza út.", 0.1);
+            Console.Clear();
+            lassuKiirás($"\n\tLe kell győznöd! {jelenlegiJatekos.Nev} Értetted?", 0.1);
+            lassuKiirás("\n\tAmint készen állsz nyomj meg egy billentyűt!", 0.1);
+            Console.ReadKey();
+            Console.Clear();
         }
     }
 
